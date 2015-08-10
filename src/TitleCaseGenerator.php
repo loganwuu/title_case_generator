3e302c8d39c2817c3d238ea16680d88ea86a8c34
logan wu & austin blanchard <student@epicodus.com>
2015-08-10 15:10:55 -0700

    capitalize first word.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -9,7 +9,7 @@
             $output_titlecased = array();
             $special_words = array("on", "the");
             foreach ($new_array_of_words as $word) {
-                if (in_array($word, $special_words)) {
+                if (in_array($word, $special_words) && $word != $new_array_of_words[0]) {
                     array_push($output_titlecased, $word);
                 } else {
                     array_push($output_titlecased, ucfirst($word));
