bbecc0133f47bbe44f029ebecb3ae23e499b5e86
Logan Wu <loganwuu@gmail.com>
2015-08-10 15:54:19 -0700

    add more special words.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -7,7 +7,7 @@
             $lowercase_words = strtolower($input_title);
             $new_array_of_words = explode(" ", $lowercase_words);
             $output_titlecased = array();
-            $special_words = array("on", "the");
+            $special_words = array("on", "the", "to", "from");
             foreach ($new_array_of_words as $word) {
                 if (in_array($word, $special_words) && $word != $new_array_of_words[0]) {
                     array_push($output_titlecased, $word);
